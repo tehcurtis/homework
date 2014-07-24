e5ed44cdfa937e007d1b35d69ec10a59a0ff2495
Curtis Edmond <curtis.edmond@gmail.com>
2014-07-24 15:46:51 -0600

    Default to an array for keys in this hash
    * So we don't have to fuss with doing it ourselves.

--- organizations/user.rb
+++ organizations/user.rb
@@ -3,14 +3,13 @@ class User
 
   def initialize
     # role: [orgs], role: [orgs]
-    @roles_and_orgs = {}
+    @roles_and_orgs = Hash.new {|hash, key| hash[key] = []}
   end
 
   def add_role_and_org(role, org)
     if roles_and_orgs.has_key?(role.name.downcase)
       roles_and_orgs << org
     else
-      roles_and_orgs[role.name] = []
       roles_and_orgs[role.name] << org
     end
   end
