6116fe60ac923bc44ed1c92943386e388a6aac66
Curtis Edmond <curtis.edmond@gmail.com>
2014-07-25 01:12:37 -0600

    Add a couple of user specs

--- spec/organizations/user_spec.rb
+++ spec/organizations/user_spec.rb
@@ -1,3 +1,17 @@
 RSpec.describe User do
+  it 'defaults roles_and_orgs to a hash' do
+    expect(User.new.roles_and_orgs).to eq({})
+  end
 
+  describe '#add_role_and_org' do
+    it 'adds the given role and org to the hash' do
+      user = User.new
+      role = Role.new('admin')
+      org = Org.new
+
+      user.add_role_and_org(role, org)
+
+      expect(user.roles_and_orgs).to include({'admin' => [org]})
+    end
+  end
 end
